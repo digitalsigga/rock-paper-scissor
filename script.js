6845e81794de00c472828bd9f23abf4dd8b52f4d
Sigrídur Birna Matthíasdóttir <sigridurbirnamatthiasdottir@Sigridurs-MacBook-Pro.local>
2023-12-15 14:37:52 +0000

    feat : created a rock, paper, scissor program

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    var choices = ['rock', 'paper', 'scissors'];
+    var randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function playGame (playerSelection, computerSelection) {
+    if playerSelection 
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playGame(playerSelection, computerSelection));
